8c256a76c3e380e0e7db22b37847daabefa18efc
toger <togerthe@naver.com>
2025-05-05 15:56:15 +0900

    init backend

---
+++ src/main/java/com/turnon/safety/SafetyApi/SafetyApiApplication.java
@@ -0,0 +1,13 @@
+package com.turnon.safety.SafetyApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafetyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafetyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/turnon/safety/SafetyApi/SafetyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turnon.safety.SafetyApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafetyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
